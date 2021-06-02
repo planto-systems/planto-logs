a48831a29d6ebf5af0afaea29f7d709a808da6d4
Planto Systems <planto.systems.inc@gmail.com>
2021-06-02 19:52:01 +0200

    Wed 02 Jun 2021 07:52:01 PM CEST

--- pushLogsToGithub.sh
+++ pushLogsToGithub.sh
@@ -1,6 +1,7 @@
 #/bin/sh
-git add *
+cd /home/pi/githubRepos/planto-logs
+/usr/bin/git add *
 commit_message=$(date)
-echo "$commit_message"
-git commit -m "${commit_message}"
-git push
+echo "$commit_message">>/home/pi/log.txt
+/usr/bin/git commit -m "${commit_message}" -a
+/usr/bin/git push>>/home/pi/log.txt 2>&1

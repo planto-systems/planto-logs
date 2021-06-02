a7190ebb45b5a41a090107eebe886f79858be7ae
Planto Systems <planto.systems.inc@gmail.com>
2021-06-02 20:30:01 +0200

    Wed 02 Jun 2021 08:30:01 PM CEST

--- rotateLogs.sh
+++ rotateLogs.sh
@@ -1,6 +1,12 @@
 #/bin/sh
 logpath=/home/pi/githubRepos/planto-logs/
 logarchivepath=${logpath}archive/
+sg1Log=sg1.log
+sg2Log=sg2.log
+
 yesterday=$(date -d "1 day ago" '+%Y-%m-%d')
-mv ${logpath}sg1.log $logarchivepath${yesterday}_sg1.log
-touch {logpath}sg1.log
+
+mv ${logpath}sg1.log $logarchivepath${yesterday}_$sg1Log
+touch {logpath}$sg1Log
+mv ${logpath}$sg2Log $logarchivepath${yesterday}_$sg2Log
+touch {logpath}$sg2Log
